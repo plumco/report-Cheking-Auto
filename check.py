5bb4afa77a77e87c9d5acf5de7ec08f8f60abbb0
Umesh Nikam <50052472+plumco@users.noreply.github.com>
2026-04-10 19:54:22 +0530

    Delete check.py

--- check.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAWfBkPc1n2VeJfGrmkskLMRg6YxhHj4QQ")
-
-print("YOUR AVAILABLE MODELS:")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
